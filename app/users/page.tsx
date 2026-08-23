aa582e6be9f2bd29389a722b2712a1b004d7b27b
Remus-21 <ferdinandremus@gmail.com>
2026-08-23 22:20:01 +0300

    Initial commit

---
+++ app/users/page.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+interface User {
+  email: string;
+  id: number;
+  name: string;
+}
+// When you don'twant to store data in cache
+// const Userspage = async () => {
+//    const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store' });
+//    const users: User[] = await res.json();
+// When getting fresh data every 10 seconds
+const Userspage = async () => {
+   const res = await fetch('https://jsonplaceholder.typicode.com/users', { next: { revalidate: 10 } });
+   const users: User[] = await res.json();
+   
+
+
+   return (
+     <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <table className='table table-bordered'>
+        <thead>
+            <tr>
+                <td>Name</td>
+                <td>Email</td>
+            </tr>
+        </thead>
+        <tbody>
+             {users.map((user) => (
+          <tr key={user.id}>
+            <th>{user.name}</th>
+            <th>{user.email}</th>
+          </tr>
+        ))}
+        </tbody>
+       
+      </table>
+     </> 
+      
+    
+)
+}
+
+export default Userspage
